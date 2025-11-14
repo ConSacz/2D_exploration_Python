0de3850a86146fe7289a8f73570f8cbcda31aa26
SonTran <102002874+ConSacz@users.noreply.github.com>
2025-11-14 21:38:33 +0700

    Add files via upload

---
+++ genarea.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+def genarea():
+    # Đọc ảnh gốc (dạng màu)
+    I_raw = cv2.imread('./maps/image3.png')  # ảnh đọc theo định dạng BGR (OpenCV mặc định)
+
+    # Resize về 101 x 101
+    I_raw = cv2.resize(I_raw, (100, 100), interpolation=cv2.INTER_AREA)
+
+    # Chuyển sang ảnh xám
+    I_gray = cv2.cvtColor(I_raw, cv2.COLOR_BGR2GRAY)
+
+    # Chuyển sang kiểu double trong khoảng [0,1]
+    Obstacle_Area = I_gray.astype(np.float64) / 255.0
+
+    # Các pixel >0 được gán là 1 (interest area), 0 giữ nguyên (obstacle area)
+    Obstacle_Area[Obstacle_Area > 0] = 1
+
+    return Obstacle_Area
